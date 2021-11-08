9604bd3f4b9f1b05d71517848afea41025216043
Emeli Gabriela Casemiro Aderaldo <emeliaderaldo@alu.ufc.br>
2021-11-08 13:18:15 -0300

    up2

--- 04_lapiseira/lapiseira.ts
+++ 04_lapiseira/lapiseira.ts
@@ -28,60 +28,99 @@ class Grafite {
 
 //agregação
 class Lapiseira {
+    grafites: Grafite[]; 
     calibre: number;
-    private grafite: Grafite | null;
+    bico: Grafite[];
+    tambor: Grafite[];
+    grafite: Grafite | null;
 
-    constructor(calibre: number) { //é a lapiseira que cria o grafite?
+    constructor(calibre: number, grafite: Grafite) { //é a lapiseira que cria o grafite?
         this.calibre = calibre;
-        this.grafite = null;
+        this.grafite = grafite;
+        this.grafites = [];
+        this.bico = [];
+        this.tambor = [];
+        
     }
 
+// grafite eh colocado como ultimo no tambor
+
     setGrafite(grafite: Grafite): boolean {
-        if (this.grafite != null) {
-            console.log("A lapiseira já possui um grafite");
+        if (this.grafites != null) {
+            this.tambor.push(this.grafites[1]) 
+            console.log("O grafitte foi colocado no tambor");
             return false;
         }
         if (grafite.calibre != this.calibre) {
             console.log("O grafite não é compatível com a lapiseira");
             return false;
         }
-        this.grafite = grafite;
+        this.grafites.push(grafite);
+        //colocar o grafite no vetor tambor
         return true;
     }
 
+    
     removerGrafite(): Grafite | null {
-        if (this.grafite == null) {
+        if (this.grafites.length == 0) {
             console.log("A lapiseira não possui um grafite");
             return null;
         }
-        let grafite = this.grafite;
-        this.grafite = null;
+        let grafite = this.grafites.shift();
+        this.grafites = null;
         return grafite;
+    }
+
+    puxarGrafite(): Grafite | boolean{
+        if (this.grafites.length != null) {
+            this.removerGrafite();
+            this.bico.push(this.tambor[0]);
+        
+           // return this.bico;
+        return true;
+        }
+        //Puxa o grafite do tambor para o bico.
+        // Se já tiver um grafite no bico, esse precisa ser removido primeiro.
     }
 
     escrever(folhas: number): boolean {
         //verificar se existe grafite
-        if (this.grafite == null) {
+        if (this.grafites == null) {
             console.log("A lapiseira não possui um grafite");
             return false;
         }
         let gasto = this.grafite.gastoPorFolha() * folhas;
-        if (gasto <= this.grafite.tamanho) {
+        if (gasto <= this.grafites[0].tamanho) {
             console.log("Escrita concluida");
-            this.grafite.tamanho -= gasto;
+            this.grafites[0].tamanho -= gasto;
         } else {
-            let realizado = this.grafite.tamanho / this.grafite.gastoPorFolha()
+            let realizado = this.grafites[0].tamanho / this.grafite.gastoPorFolha()
             console.log("Escrita parcial: " + realizado + " folhas");
-            this.grafite.tamanho = 0;
+            this.grafites[0].tamanho = 0;
         }
-        if (this.grafite.tamanho == 0) {
-            this.grafite = null;
+        if (this.grafites[0].tamanho <= 10) {
+            this.grafites = null;
         }        
     }
+    
+    toString(): string {
+        if (this.grafites.length == 0){
+            return "grafite vazia"
+        }
+        let saida = `(bico: ${this.grafites[0].calibre}:${this.grafites[0].dureza}:${this.grafites[0].tamanho}) [ `;
+        for (let i = 1; i < this.grafites.length; i++){
+            saida += `tambor: ${this.grafites[i].calibre}:${this.grafites[i].dureza}:${this.grafites[i].tamanho} `;
+        }
+        return saida + "]";
+    }
+
 }
 
-let pentel = new Lapiseira(0.5);
+let pentel = new Lapiseira(0.5,(new Grafite(0.4, "HB", 60)) );
 pentel.setGrafite(new Grafite(0.5, "HB", 40));
+pentel.setGrafite(new Grafite(0.9, "2HB", 50));
+pentel.setGrafite(new Grafite(0.7, "6HB", 30));
+pentel.puxarGrafite();
 pentel.escrever(10);
 pentel.escrever(40);
 console.log(pentel);
